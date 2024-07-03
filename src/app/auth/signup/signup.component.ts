7d8c615b427fce57fa48d1ac6f54807a1c4890a5
ana-antic-singi <ana.antic.21@singimail.rs>
2024-07-04 01:55:04 +0200

    implementiran sign up, users iz user service se čuvaju u local storage

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,7 +10,26 @@ import { NgForm } from '@angular/forms';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+
+  constructor(
+    private userService: UserService, 
+    private router: Router, 
+    private authService: AuthService){}
+    
   onSubmit(form: NgForm) {
-    console.log(form);
+    const email = form.value.email;
+    console.log(email);
+
+    if (this.userService.emailExists(email)){
+        window.alert("Email taken!")
+        console.log("zauzet mejl");
+    }
+    else{
+      console.log("ok mejl");
+      const password = form.value.password;
+      const cardNumber = form.value.cardNumber;
+      this.userService.newUser(password, email, cardNumber)
+      this.router.navigate(['/login']);
+    }
   }
 }
--- src/app/auth/user.service.ts
+++ src/app/auth/user.service.ts
@@ -2,22 +2,52 @@ import { User } from "./user.model";
 
 // dodaj u providers u koreni modul!
 export class UserService {
-    private users : User [] =  [
-        {
-            id: "0",
-            username: "anchy",
-            password:"123",
-            email: "anchy@gmail.com",
-            cardNumber: 123
-        },
-        {
-            id: "1",
-            username: "ana",
-            password:"123",
-            email: "ana@gmail.com",
-            cardNumber: 456
+    // private users : User [] =  [
+    //     {
+    //         id: "0",
+    //         password:"123",
+    //         email: "anchy@gmail.com",
+    //         cardNumber: 123
+    //     },
+    //     {
+    //         id: "1",
+    //         password:"123",
+    //         email: "ana@gmail.com",
+    //         cardNumber: 456
+    //     }
+    // ]
+    private users: User[] = [];
+
+    constructor() {
+        this.loadUsersFromLocalStorage();
+    }
+
+    private loadUsersFromLocalStorage() {
+        const users = localStorage.getItem('users');
+        if (users) {
+            this.users = JSON.parse(users);
+        } else {
+            this.users = [
+                {
+                    id: "0",
+                    password: "123",
+                    email: "anchy@gmail.com",
+                    cardNumber: 123
+                },
+                {
+                    id: "1",
+                    password: "123",
+                    email: "ana@gmail.com",
+                    cardNumber: 456
+                }
+            ];
+            this.saveUsersToLocalStorage();
         }
-    ]
+    }
+
+    private saveUsersToLocalStorage() {
+        localStorage.setItem('users', JSON.stringify(this.users));
+    }
 
     getUsers(){
         return this.users;
@@ -25,5 +55,24 @@ export class UserService {
     userExists(email: string, password: string): boolean {
         return this.users.some(user => user.email === email && user.password === password);
       }
+    emailExists(email: string): boolean {
+        return this.users.some(user => user.email === email);
+    }
+
+    newUser(password: string, email: string, cardNumber: number) {
+
+        const lastId = Math.max(...this.users.map(user => parseInt(user.id)));
+        const newId = (lastId + 1).toString();
+
+        const newUser: User = {
+            id: newId,
+            password: password,
+            email: email,
+            cardNumber: cardNumber
+        };
+
+        this.users.push(newUser);
+        this.saveUsersToLocalStorage();
+    }
 }
 
